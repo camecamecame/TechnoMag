3ca89a14bfa4e781691eb4fcde13d9b316c1a040
camecamecame <157813937+camecamecame@users.noreply.github.com>
2026-04-16 09:39:36 +0300

    Update delete_item.php

--- delete_item.php
+++ delete_item.php
@@ -2,28 +2,28 @@
 session_start();
 require '../db.php';
 
-/* Проверка авторизации */
+
 if (!isset($_SESSION['user_id'])) {
     die("Доступ запрещен");
 }
 
-/* Только администратор может удалять */
+
 if ($_SESSION['user_role'] !== 'admin') {
     die("Доступ запрещен");
 }
 
 
-/* Проверяем id */
+
 if (!isset($_POST['id']) || !is_numeric($_POST['id'])) {
     die("Ошибка ID");
 }
 
 $id = (int)$_POST['id'];
 
-/* Удаление */
+
 $stmt = $pdo->prepare("DELETE FROM products WHERE id = ?");
 $stmt->execute([$id]);
 
-/* Возврат обратно */
+
 header("Location: index.php");
 exit;
